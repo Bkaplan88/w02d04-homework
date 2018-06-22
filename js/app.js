b2d55a3f19acbe65834d7c1a8e24fbdbff1d18e4
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-21 19:57:43 -0600

    added my first element and it was fun

--- js/app.js
+++ js/app.js
@@ -1,3 +1,63 @@
 console.log("I did it! I linked jQuery and this file!");
 
 
+ $(() => {
+
+ 	const $container = $("#container");
+ 	const $h1 = $("<h1></h1>");
+ 	$("body").append($h1);
+
+ 	$h1.text("Hogwarts");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
