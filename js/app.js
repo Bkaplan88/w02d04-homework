217b08a7909e5fc3c1ddc6d45659bdd2fa1cd9b3
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-21 20:40:10 -0600

    I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- js/app.js
+++ js/app.js
@@ -18,7 +18,23 @@ console.log("I did it! I linked jQuery and this file!");
  	const $wand = $("<h4>Mahagony Wand with Frog Guts Core</h4>"); 
  	$pet.addClass("cat");
 	$container.append($wand);
-  	
+
+	const $storageTrunk = $("<ul></ul>");
+	$storageTrunk.attr("storage", "trunk");
+	console.log($storageTrunk);
+	const $butterBeer = $("<li>butter beer</li>");
+	const $invisibilityCloak = $("<li>invisibility cloak</li>");
+	$invisibilityCloak.addClass("secret");
+	const $magicMap = $("<li>magic map</li>");
+	$magicMap.addClass("secret");
+	const $timeTurner = $("<li>time turner</li>");
+	$timeTurner.addClass("secret");
+	const $leash = $("<li>leash</li>");
+	$leash.addClass("cat");
+	const $jellyBeans = $("<li>Bertie Bott's Every Flavor Beans</li>");
+	$storageTrunk.append($butterBeer, $invisibilityCloak, $magicMap, $timeTurner, $leash, $jellyBeans );
+	$container.append($storageTrunk);
+
 
  
 
